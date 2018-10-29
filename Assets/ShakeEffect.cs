0f8f650529f3a5211030297038d8e72cdf9253b7
Paul Sinnett <paul.sinnett@gmail.com>
2018-10-29 20:57:54 +0000

    Initial commit

---
+++ Assets/ShakeEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeEffect : MonoBehaviour
+{
+	public CameraShake shake;
+	public AnimationCurve amplitudeCurve;
+	public float amplitudeMultiplier = 1.0f;
+	public Vector2 frequency = Vector2.zero;
+	float time = 0.0f;
+
+	public float Duration(AnimationCurve curve)
+	{
+		return curve.keys[curve.length - 1].time;
+	}
+
+	public float Length()
+	{
+		return Duration(amplitudeCurve);
+	}
+
+	void OnEnable()
+	{
+		time = 0.0f;
+	}
+
+	void Update()
+	{
+		time += Time.deltaTime;
+		if (time < Length())
+		{
+			shake.amplitude = amplitudeCurve.Evaluate(time) * amplitudeMultiplier;
+			shake.frequency = frequency;
+		}
+		else
+		{
+			shake.amplitude = 0.0f;
+			enabled = false;
+		}
+	}
+}
---
+++ Assets/TriggerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public Behaviour effect;
+    public KeyCode key;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(key))
+        {
+            effect.enabled = true;
+        }
+    }
+}
